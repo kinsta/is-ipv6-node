62db443197bf4a173db0a96f9730d71dc2235edb
Anatoliy Gatt <anatoliy.gatt@aol.com>
2015-09-23 09:06:30 +0200

    Replace " with ' in definitions of all strings in is-ipv6.js tests

--- test/is-ipv6.js
+++ test/is-ipv6.js
@@ -1,14 +1,14 @@
-var assert = require("assert");
-var isIPv6 = require("../lib/is-ipv6");
+var assert = require('assert');
+var isIPv6 = require('../lib/is-ipv6');
 
-describe("is-ipv6", function() {
-    describe("init", function() {
-        it("should return true if String is an IPv6 address", function() {
-            assert.equal(isIPv6("2a02:2770::21a:4aff:feb3:2ee"), true, "String should be an IPv6 address");
+describe('is-ipv6', function() {
+    describe('init', function() {
+        it('should return true if String is an IPv6 address', function() {
+            assert.equal(isIPv6('2a02:2770::21a:4aff:feb3:2ee'), true, 'String should be an IPv6 address');
         });
 
-        it("should return false if String is not an IPv6 address", function() {
-            assert.equal(isIPv6("Hello World!"), false, "String should not be an IPv6 address");
+        it('should return false if String is not an IPv6 address', function() {
+            assert.equal(isIPv6('Hello World!'), false, 'String should not be an IPv6 address');
         });
     });
 });

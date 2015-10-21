37cf15315c6b9a13548c4803c23ad9370a27d85e
Anatoliy Gatt <anatoliy.gatt@aol.com>
2015-10-21 12:44:34 +0200

    Fix anonymous functions declaration coding style in is-ipv6.js tests

--- test/is-ipv6.js
+++ test/is-ipv6.js
@@ -3,11 +3,11 @@ var isIPv6 = require('../lib/is-ipv6');
 
 describe('is-ipv6', function () {
     describe('init', function () {
-        it('should return true if String is an IPv6 address', function() {
+        it('should return true if String is an IPv6 address', function () {
             assert.equal(isIPv6('2a02:2770::21a:4aff:feb3:2ee'), true, 'String should be an IPv6 address');
         });
 
-        it('should return false if String is not an IPv6 address', function() {
+        it('should return false if String is not an IPv6 address', function () {
             assert.equal(isIPv6('Hello World!'), false, 'String should not be an IPv6 address');
         });
     });
